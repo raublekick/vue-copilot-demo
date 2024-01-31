75d65a274db836895f05e2c2074fadbc17b6d20b
Andrew Raub <raublekick@gmail.com>
2024-01-31 14:50:14 -0700

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+
+<script setup>
+</script>
+
+
+<template>
+  <main>
+    <h1>Goals</h1>
+
+    <dl>
+      <dt>Goal #1</dt>
+      <dd>Create an array of mock users with name and email using the Copilot chat plugin</dd>
+    </dl>
+
+    <dl>
+      <dt>Goal #2</dt>
+      <dd>Create a table to display the users using a comment</dd>
+    </dl>
+
+    <dl>
+      <dt>Goal #3</dt>
+      <dd>Add some basic styling (borders and padding) to the table</dd>
+    </dl>
+
+    <dl>
+      <dt>Goal #4</dt>
+      <dd>Add filtering the users using terminal commands</dd>
+    </dl>
+
+    <dl>
+      <dt>Goal #5</dt>
+      <dd>Add a new field to the users table</dd>
+    </dl>
+
+    <dl>
+      <dt>Goal #6</dt>
+      <dd>Use Copilot to explain the computed filter function.</dd>
+    </dl>
+
+    <dl>
+      <dt>Goal #7</dt>
+      <dd>Use Copilot to document the computed filter function.</dd>
+    </dl>
+  </main>
+</template>
